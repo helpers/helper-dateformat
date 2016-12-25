fa986e9240e49faf5acee4a9ad511b8dc7978a7a
jonschlinkert <jon.schlinkert@sellside.com>
2016-12-25 00:06:21 -0500

    use assert

--- test.js
+++ test.js
@@ -7,11 +7,11 @@
 
 'use strict';
 
-var should = require('should');
+require('mocha');
+var assert = require('assert');
 var date = require('dateformat');
 var handlebars = require('handlebars');
 var _ = require('lodash');
-
 var helper = require('./');
 
 function dateformat(format) {
@@ -19,26 +19,28 @@ function dateformat(format) {
 }
 
 describe('dateformat', function () {
-  it('should return a formatted moment date:', function () {
-    helper().should.eql(dateformat('mmmm dd, yyyy'));
+  it('should return a formatted date:', function () {
+    assert.deepEqual(helper(), dateformat('mmmm dd, yyyy'));
+    assert.deepEqual(helper(new Date()), dateformat('mmmm dd, yyyy'));
   });
 
   it('should return a formatted dateformat date:', function () {
-    helper('mmmm dd, yyyy').should.eql(dateformat('mmmm dd, yyyy'));
+    assert.deepEqual(helper('mmmm dd, yyyy'), dateformat('mmmm dd, yyyy'));
+    assert.deepEqual(helper(new Date(), 'mmmm dd, yyyy'), dateformat('mmmm dd, yyyy'));
   });
 
   it('should work as a lodash helper:', function () {
-    _.template('<%= date("mmmm dd, yyyy") %>', {}, {imports: {date: helper}}).should.eql(dateformat('mmmm dd, yyyy'));
-    _.template('<%= date("mmmm") %>', {}, {imports: {date: helper}}).should.eql(dateformat('mmmm'));
-    _.template('<%= date("mmmm dd, yyyy") %>', {}, {imports: {date: helper}}).should.not.eql(dateformat('mmmm'));
+    assert.deepEqual(_.template('<%= date("mmmm dd, yyyy") %>', {}, {imports: {date: helper}}), dateformat('mmmm dd, yyyy'));
+    assert.deepEqual(_.template('<%= date("mmmm") %>', {}, {imports: {date: helper}}), dateformat('mmmm'));
+    assert.notDeepEqual(_.template('<%= date("mmmm dd, yyyy") %>', {}, {imports: {date: helper}}), dateformat('mmmm'));
   });
 
   it('should work as a handlebars helper:', function () {
     handlebars.registerHelper('date', helper);
 
-    handlebars.compile('{{date "mmmm dd, yyyy"}}')().should.eql(dateformat('mmmm dd, yyyy'));
-    handlebars.compile('{{date "mmmm"}}')().should.eql(dateformat('mmmm'));
-    handlebars.compile('{{date "mmmm"}}')().should.not.eql(dateformat('mmmm dd, yyyy'));
+    assert.deepEqual(handlebars.compile('{{date "mmmm dd, yyyy"}}')(), dateformat('mmmm dd, yyyy'));
+    assert.deepEqual(handlebars.compile('{{date "mmmm"}}')(), dateformat('mmmm'));
+    assert.notDeepEqual(handlebars.compile('{{date "mmmm"}}')(), dateformat('mmmm dd, yyyy'));
   });
 });
 
